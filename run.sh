3b52afc4a0022257b327d8f35a8b12339b4220fc
Daniele Dapuzzo <daniele.dapuzzo92@gmail.com>
2022-07-05 21:51:33 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+echo Reading environment
+if [ -f .env ]; then
+    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+else
+    echo No env file found, trying with system env variables
+fi
+echo Using profile $PROFILE_NAME
+echo Running docker container
+docker run -it -v ~/.aws:/home/glue_user/.aws -v $JUPYTER_WORKSPACE_LOCATION:/home/glue_user/workspace/jupyter_workspace/ -e AWS_PROFILE=$PROFILE_NAME -e DISABLE_SSL=true --rm -p 4040:4040 -p 18080:18080 -p 8998:8998 -p 8888:8888 --name glue_jupyter_lab amazon/aws-glue-libs:glue_libs_2.0.0_image_01 /home/glue_user/jupyter/jupyter_start.sh
